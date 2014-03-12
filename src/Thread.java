d3a6cf5db15a0b7186861fa8fe384e442c345c80
T. Alexander <t.alex@outlook.com>
2014-03-12 09:26:20 -0400

    Update Thread.java
    Added some more logistic stuff

--- src/Thread.java
+++ src/Thread.java
@@ -1,14 +1,34 @@
-import java.util.Calendar;
+import java.util.*; import java.io.*;
 
 public class Thread {
 	public static String StartCommand = "java -version";
 	public static String EndCommand = "java -version";
 	public static String HiberCommand = "java -version";
 	
+	public static int onTimeHour, onTimeMinute, offTimeHour, offTimeMinute;
+	
 	public static void main(String args[]) {
 		Display x = new Display();
 		x.Output.setText("AMDGS Software 2.0");
+		
+		Log log = new Log();
 		boolean serverRunning = false;
+		log.write("Server set to default state: off");
+		
+		try {
+			Scanner in = new Scanner(new File("Schedule.txt"));
+			onTimeHour = in.nextInt();
+			onTimeMinute = in.nextInt();
+			offTimeHour = in.nextInt();
+			offTimeMinute = in.nextInt();
+			log.write("Successfully read Schedule.txt configuration.");
+			in.close(); }
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+			log.write("ERROR: unable to read Schedule.txt configuration."); }
+		
+		log.write("Loading finished.");
+		log.write("Main loop initialized.");
 		while (true) {
 			if (currentHour() == 9 && currentMinute() == 5 && !serverRunning) serverRunning = true;
 			if (currentHour() == 9 && currentMinute() == 6 && serverRunning) serverRunning = false;
@@ -26,6 +46,10 @@ public class Thread {
 	
 	public static int currentHour() {return Calendar.getInstance().get(Calendar.HOUR_OF_DAY); }
 	public static int currentMinute() {return Calendar.getInstance().get(Calendar.MINUTE); }
+	
+	public static int currentDay() {return Calendar.getInstance().get(Calendar.DAY_OF_MONTH); }
+	public static int currentMonth() {return Calendar.getInstance().get(Calendar.MONTH) + 1; }
+	public static int currentYear() {return Calendar.getInstance().get(Calendar.YEAR); }
 
 	
 	

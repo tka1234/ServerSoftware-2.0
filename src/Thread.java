31ecc2da522f3d3bf6aee7adf4c3cc5aeecbfd72
T. Alexander <t.alex@outlook.com>
2014-03-13 09:25:33 -0400

    Update Thread.java
    Added preliminary stuff for on/off state detection

--- src/Thread.java
+++ src/Thread.java
@@ -1,17 +1,17 @@
 import java.util.*; import java.io.*;
-
 public class Thread {
 	public static String StartCommand = "java -version";
 	public static String EndCommand = "java -version";
 	public static String HiberCommand = "java -version";
-	
 	public static int onTimeHour, onTimeMinute, offTimeHour, offTimeMinute;
+	public static boolean override = false;
+	public static Log log;
 	
 	public static void main(String args[]) {
 		Display x = new Display();
 		x.Output.setText("AMDGS Software 2.0");
 		
-		Log log = new Log();
+		log = new Log();
 		boolean serverRunning = false;
 		log.write("Server set to default state: off");
 		
@@ -29,12 +29,23 @@ public class Thread {
 		
 		log.write("Loading finished.");
 		log.write("Main loop initialized.");
+		int currentDay = 0;
 		while (true) {
-			if (currentHour() == 9 && currentMinute() == 5 && !serverRunning) serverRunning = true;
-			if (currentHour() == 9 && currentMinute() == 6 && serverRunning) serverRunning = false;
+			if (currentHour() < offTimeHour) serverRunning = true;
+			else if (currentHour() == offTimeHour && currentMinute() < offTimeMinute) serverRunning = true;
+			else if (currentHour() < onTimeHour) serverRunning = false;
+			else if (currentHour() == onTimeHour && currentMinute() < onTimeMinute) serverRunning = false;
 			
+			if (override) serverRunning = true;
+		
 			if (serverRunning) x.Status.setText("The server is currently running.");
 			else x.Status.setText("The server is off and hibernating.");
+			
+			if (currentDay() != currentDay) {
+				currentDay = currentDay();
+				log.write(" ");
+				log.write(currentMonth() + "/" + currentDay() + "/" + currentYear() + " -");
+			}
 		}
 		
 		//runCommand("java -jar F:/TST/Flatspace.jar");
@@ -46,11 +57,7 @@ public class Thread {
 	
 	public static int currentHour() {return Calendar.getInstance().get(Calendar.HOUR_OF_DAY); }
 	public static int currentMinute() {return Calendar.getInstance().get(Calendar.MINUTE); }
-	
 	public static int currentDay() {return Calendar.getInstance().get(Calendar.DAY_OF_MONTH); }
 	public static int currentMonth() {return Calendar.getInstance().get(Calendar.MONTH) + 1; }
 	public static int currentYear() {return Calendar.getInstance().get(Calendar.YEAR); }
-
-	
-	
 }
